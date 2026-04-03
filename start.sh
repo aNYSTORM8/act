6fd0eddaa43925c111539a3b5a609b6e38443a20
Automated User <automated@example.com>
2026-04-03 10:24:18 -0500

    holaxxx

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Hacer login
 /app/bitpingd login --email "margretsapphire@sharebot.net" --password "K2,)td+X&=R9sL+"
-echo "Container alive, starting ping..."
-ping "$PING_TARGET"
+
+echo "Container alive, starting Bitping daemon..."
+# Ejecutar el daemon en foreground
+/app/bitpingd daemon --data-dir /root/.bitpingd
